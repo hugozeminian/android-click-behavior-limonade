294f78bfd17b4690386499ac920284e4c0272e21
hugozaminian <hugozamian@hotmail.com>
2024-02-01 17:39:26 -0700

    text on botton working

--- app/src/main/java/com/example/click_behavior/MainActivity.kt
+++ app/src/main/java/com/example/click_behavior/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
@@ -22,16 +23,22 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.click_behavior.ui.theme.Click_behaviorTheme
+import org.w3c.dom.Text
 
 
 class MainActivity : ComponentActivity() {
@@ -107,25 +114,61 @@ fun BodyContainer(modifier: Modifier){
         modifier = modifier
     )
     {
-        Button(onClick = { /*TODO*/ },
+        var step by remember { mutableStateOf(1) }
+
+        Button(
+            onClick = {
+                step ++
+                if(step > 4){
+                    step = 1
+                }
+                      },
             shape = RoundedCornerShape(32.dp),
             colors = ButtonDefaults.buttonColors(AppTheme.BackgroudGreen),
-            modifier = Modifier.size(256.dp)
+            modifier = Modifier
+                .size(256.dp)
+                .align(Alignment.CenterHorizontally),
             ) {
             Image(painter = painterResource(id = R.drawable.lemon_drink),
                 contentDescription = null
             )
         }
-
-        Text(text = "phrase",
-            modifier = Modifier
-                .padding(top = AppTheme.PaddingLarge)
-                .align(Alignment.CenterHorizontally),
-            fontSize = AppTheme.SizeFontMedium
-        )
+        LemonadeFlow(step)
     }
 }
 
-fun LemonadeFlow(){
+@Composable
+fun LemonadeFlow(step: Int){
+    when (step){
+        1 -> {
+            LemonadeText(textInstruction = R.string.step_1)
+        }
 
+        2 -> {
+            LemonadeText(textInstruction = R.string.step_2)
+        }
+
+        3 -> {
+            LemonadeText(textInstruction = R.string.step_3)
+        }
+
+        4 -> {
+            LemonadeText(textInstruction = R.string.step_4)
+        }
+    }
+}
+
+@Composable
+fun LemonadeText(textInstruction: Int){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = AppTheme.PaddingLarge)
+            .wrapContentSize(Alignment.Center),
+    ) {
+        Text(
+            text = stringResource(id = textInstruction),
+            fontSize = AppTheme.SizeFontMedium
+        )
+    }
 }
